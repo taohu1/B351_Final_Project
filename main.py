0e5fab9dfdd6cead7fccba7b0ec384b10189909a
Sam Soucie <ssoucie@iu.edu>
2020-11-09 16:05:27 -0600

    some efficiency/performance testing

--- main.py
+++ main.py
@@ -1,25 +1,41 @@
 from board import *
 from player import *
 if __name__ == "__main__":
-    num_moves = 0
-    b = board()
-    p1 = RandomPlayer(b)
-    # p2 = ManualPlayer(b)
-    #p2 = RandomPlayer(b)
-    p2 = AlphaBetaPlayer(b,4)
-    b.print_board()
-    while not b.game_over():
+    randomwins = 0
+    abwins = 0
+    totalmoves = 0
+    start_time = time.time()
+    for i in range(1000): 
+      num_moves = 0
+      b = board()
+      p1 = RandomPlayer(b)
+      # p2 = ManualPlayer(b)
+      #p2 = RandomPlayer(b)
+      p2 = AlphaBetaPlayer(b,4)
+      #b.print_board()
+      while not b.game_over():
+        if b.turn == 1:
+          pos, mv = p1.find_move()
+        else:
+          pos, mv = p2.find_move()
+        b.make_move(pos, mv)
+        num_moves += 1
       if b.turn == 1:
-        pos, mv = p1.find_move()
+        abwins += 1
       else:
-        pos, mv = p2.find_move()
-      b.make_move(pos, mv)
-      num_moves += 1
-      b.print_board()
+        randomwins += 1
+      totalmoves += num_moves
+
+    elapsed_time = time.time() - start_time
+
+    print("Out of 1000 games, the AlphaBeta AI won " + str(abwins) + " of them.")
+    print("The average number of moves was " + str(totalmoves/1000) + ".")
+    print("The total amount of time to play the 1000 games is " + str(elapsed_time) + ".")
+      # b.print_board()
       # time.sleep(1)
-    print("                   \\|||/")
-    print("                   (o o)")
-    print("****************ooO*(_)*Ooo*********************\n")
-    print("******** Game Over! Player " + str(b.turn) + " has lost. *********\n")
-    print("********** That game took " + str(num_moves) + " turns. ************\n")
-    print("************************************************\n")
+    #print("                   \\|||/")
+    #print("                   (o o)")
+    #print("****************ooO*(_)*Ooo*********************\n")
+    #print("******** Game Over! Player " + str(b.turn) + " has lost. *********\n")
+    #print("********** That game took " + str(num_moves) + " turns. ************\n")
+    #print("************************************************\n")
